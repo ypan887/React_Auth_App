7b7bdc0ebec364ebdb70eebe5c96fc6069149d94
ypan887 <ypan887@gmail.com>
2016-12-30 15:28:36 -0800

    add fetch library

--- cfg/dist.js
+++ cfg/dist.js
@@ -10,7 +10,7 @@ let defaultSettings = require('./defaults');
 let BowerWebpackPlugin = require('bower-webpack-plugin');
 
 let config = Object.assign({}, baseConfig, {
-  entry: path.join(__dirname, '../src/index'),
+  entry: ['whatwg-fetch', path.join(__dirname, '../src/index')],
   cache: false,
   devtool: 'sourcemap',
   plugins: [
